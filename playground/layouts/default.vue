07ba7ef6cd5689a681f73dd1c8bb519d85cc152c
Suche Kahanamoku <sukusovi@gmail.com>
2025-11-20 17:54:57 +0100

    Update

--- playground/layouts/default.vue
+++ playground/layouts/default.vue
@@ -16,20 +16,8 @@ const isOpen = ref(true);
 
 <template>
   <div class="min-h-screen flex flex-col">
-    <CmpHeader />
-
-    <CmpSlideMenu
-      v-model:is-open="isOpen"
-      :menu-items="menuItems"
-      :config="{ side: 'right', ui: { title: 'h-8' } }"
-    >
-      <template #logo> Logo </template>
-    </CmpSlideMenu>
-
     <main>
       <slot></slot>
     </main>
-
-    <CmpFooter url="https://www.prasentace.cz/" name="PRASENTACE" />
   </div>
 </template>
